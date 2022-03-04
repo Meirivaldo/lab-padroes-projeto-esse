7518109481a5d09d1f69a216de666960748f18c9
Meirivaldo <95186840+Meirivaldo@users.noreply.github.com>
2022-03-03 23:19:12 -0300

    Initial commit

---
+++ src/main/java/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,33 @@
+package service.impl;
+
+import model.Cliente;
+import service.ClienteService;
+
+public class ClienteServiceImpl implements ClienteService {
+
+
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+
+    }
+
+    @Override
+    public void deletar(Long id) {
+
+    }
+}
---
+++ src/test/java/one/dio/com/labpadroesprojetoesse/LabPadroesProjetoEsseApplicationTests.java
@@ -0,0 +1,13 @@
+package one.dio.com.labpadroesprojetoesse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadroesProjetoEsseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
